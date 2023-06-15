b7c0f464a30b1892b77db5f15367854ec466ee0e
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-06-15 21:06:47 +0530

    components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderModule } from './header/header.module';
+// import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    //HeaderComponent
   ],
   imports: [
-    BrowserModule  //sub module to AppModule //browserModule has basic browser features
+    BrowserModule,  //sub module to AppModule //browserModule has basic browser features
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
